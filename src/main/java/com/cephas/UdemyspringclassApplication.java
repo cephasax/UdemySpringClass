5a899f4e93427153fe1461055093a4f981ca72e3
cephasax <cephasax@gmail.com>
2018-09-13 19:55:44 -0300

    projeto criado

---
+++ src/main/java/com/cephas/UdemyspringclassApplication.java
@@ -0,0 +1,12 @@
+package com.cephas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyspringclassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyspringclassApplication.class, args);
+	}
+}
